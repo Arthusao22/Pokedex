5940a7e65c2a424bb9e71cc94191e043262062d7
Arthur <133883144+Arthusao22@users.noreply.github.com>
2026-06-12 16:13:01 -0300

    alterei js

--- src/js/source.js
+++ src/js/source.js
@@ -1,36 +1,37 @@
-document.querySelector("listPokemon")
-const searchInput = document.getElementById("searchInput")
-const button = document.getElementById("button-search")
+document.querySelector("#listPokemon");
+const searchInput = document.getElementById("searchInput");
+const button = document.getElementById("button-search");
 
-
-button.addEventListener("click", async (e) =>{
-    let search = searchInput.value
-    await buscarPokemon(search)
-})
-
+button.addEventListener("click", async (e) => {
+  let search = searchInput.value;
+  await buscarPokemon(search);
+  await buscarPokemonLista(50, 0);
+});
 
 let allPokemons = [];
 
-async function buscarPokemon(nome)  
-    {
-        try{
-            const response  = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${nome}`
-        )
+async function buscarPokemon(nome) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`);
 
-        const data = await response.json();
+    const data = await response.json();
 
-        console.log(data)
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function buscarPokemonLista (limit,offset) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
 
-        }
-
-        catch(error){
-            console.log(error.message)
-        }
-
-    
-   }
-
-
-    
