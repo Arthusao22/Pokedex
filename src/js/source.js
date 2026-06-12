2cf640dc6b58ebfbba99c5b644599b92d98b5d89
Arthur <133883144+Arthusao22@users.noreply.github.com>
2026-06-12 15:41:33 -0300

    alterei o js

--- src/js/source.js
+++ src/js/source.js
@@ -1,11 +1,35 @@
-document.querySelectorById("listPokemon")
-document.querySelectorById("")
+document.querySelector("listPokemon")
+document.querySelector("searchInput")
+const button = document.getElementById("button-search")
+
+button.addEventListener("click", async (e) =>{
+    console.log("Funciona")
+    await buscarPokemon()
+})
+
 
 let allPokemons = [];
 
-async function loadPokemon() {
-    const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon/ditto"
-    )}
+async function buscarPokemon()  
+    {
+        try{
+            const response  = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/ditto" 
+        )
+
+        const data = await response.json();
+
+        console.log(data)
+
+
+        }
+
+        catch(error){
+            console.log(error.message)
+        }
+
+    
+   }
+
 
     
