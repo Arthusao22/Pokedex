14dfd36e54ed6908975fde44ace4cd460b204b16
Guilherme <burivarusama@gmail.com>
2026-06-12 17:17:43 -0300

    listagem e img

--- src/js/source.js
+++ src/js/source.js
@@ -1,11 +1,12 @@
-document.querySelector("#listPokemon");
+const listPokemon = document.getElementById("listPokemon");
 const searchInput = document.getElementById("searchInput");
 const button = document.getElementById("button-search");
 
 button.addEventListener("click", async (e) => {
   let search = searchInput.value;
   await buscarPokemon(search);
-  await buscarPokemonLista(50, 0);
+  const pokelist = await buscarPokemonLista(5, 0);
+  listagemPokemon(pokelist);
 });
 
 let allPokemons = [];
@@ -28,10 +29,32 @@ async function buscarPokemonLista (limit,offset) {
 
     const data = await response.json();
 
-    console.log(data);
+    console.log(data.results);
+    return data.results
   } catch (error) {
     console.log(error.message);
   }
 }
 
+async function buscarPokemonURL(url) {
+  try {
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    console.log(data);
+    return data
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function listagemPokemon (pokemons){
+  listPokemon.innerHTML=""
+  for(const i in pokemons){
+    const pokemonAtual = await buscarPokemonURL(pokemons[i].url)
+    listPokemon.innerHTML+=`<p>${pokemonAtual.name}</p>`
+    listPokemon.innerHTML+=`<img src=${pokemonAtual.sprites.front_default}>`
+  };
+}
 
