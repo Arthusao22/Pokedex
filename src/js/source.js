bdf4735c284781bf8a870f6ded0a976117a92423
Arthur <133883144+Arthusao22@users.noreply.github.com>
2026-06-12 14:55:00 -0300

    mudei o css

--- src/js/source.js
+++ src/js/source.js
@@ -1,2 +1,11 @@
-document.querySelector("listPokemon")
+document.querySelectorById("listPokemon")
+document.querySelectorById("")
 
+let allPokemons = [];
+
+async function loadPokemon() {
+    const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/ditto"
+    )}
+
+    
